ea250d9cd6416515c34b8324723bfdc1251c6d71
alejandro <a.ortega6243@gmail.com>
2020-09-30 18:35:19 -0600

    radio buttons work now but the issue is that the form bracket and the submit and cancel button need to be removed

--- frontend/src/views/SetupContentForm.vue
+++ frontend/src/views/SetupContentForm.vue
@@ -1,53 +1,53 @@
 <template>
   <div>
-    <title-bar :title-stack="titleStack"/>
+    <title-bar :title-stack="titleStack" />
     <section class="section is-main-section">
-      <card-component title="Finding and Reporting Information Console (FRIC)" >
-        <form @submit.prevent="submit">
-          <notification class="is-info">
-            <div>
-              <b-icon icon="buffer" customize-size="default"/>
-              <b>There is no existing event in your local system</b>
-            </div>
-          </notification>
-          <b-field label="Please input your initials:" horizontal>
-            <b-field>
-              <b-input placeholder="Enter your initials here" required/>
-            </b-field>
+      <card-component title="Finding and Reporting Information Console (FRIC)">
+        <notification class="is-info">
+          <div>
+            <b-icon icon="buffer" customize-size="default" />
+            <b>There is no existing event in your local system</b>
+          </div>
+        </notification>
+        <b-field label="Please input your initials:" horizontal>
+          <b-field>
+            <b-input placeholder="Enter your initials here" required />
           </b-field>
-        <b-field label="Please select an option" class="has-check" horizontal>
-          <checkbox-picker :options="{new:'Create a new event (any existing event will be archived)'}" v-model="customElementsForm.checkbox"/>
+        </b-field>
+        <b-field label="Please select an option:" class="has-check" horizontal>
+          <radio-picker
+            :options="{
+              new: 'Create a new event (any existing event will be archived)',
+            }"
+            v-model="customElementsForm.radio"
+          ></radio-picker>
         </b-field>
         <b-field label="" class="has-check" horizontal>
-          <checkbox-picker :options="{sync:'First time sync with lead analyst. Please enter the lead analyst’s IP'}" v-model="customElementsForm.checkbox"/>
+          <radio-picker
+            :options="{
+              sync:
+                'First time sync with lead analyst. Please enter the lead analyst’s IP',
+            }"
+            v-model="customElementsForm.radio"
+          ></radio-picker>
         </b-field>
-          <b-field label="" horizontal>
-            <b-input placeholder="Enter initials here" required />
-          </b-field>
-          <b-field horizontal>
-            <b-field grouped>
-              <div class="control">
-                <b-button native-type="submit" type="is-primary">Submit</b-button>
-              </div>
-              <div class="control">
-                <b-button type="is-primary is-outlined" @click="cancel">Cancel</b-button>
-              </div>
-            </b-field>
-          </b-field>
-        </form>
+        <b-field label="" horizontal>
+          <b-input placeholder="Enter initials here" required />
+        </b-field>
       </card-component>
     </section>
   </div>
 </template>
 
 <script>
+import TitleBar from '@/components/TitleBar'
 import CardComponent from '@/components/CardComponent'
 import mapValues from 'lodash/mapValues'
 import Notification from '@/components/Notification'
-import CheckboxPicker from '@/components/CheckboxPicker'
+import RadioPicker from '@/components/RadioPicker'
 export default {
   name: 'SetupContentForm',
-  components: { CardComponent, Notification, CheckboxPicker },
+  components: { RadioPicker, CardComponent, Notification, TitleBar },
   data () {
     return {
       isLoading: false,
@@ -55,7 +55,7 @@ export default {
         name: null,
         email: null,
         phone: null,
-        sync_with: null,
+        department: null,
         subject: null,
         question: null
       },
@@ -65,22 +65,14 @@ export default {
         switch: true,
         file: null
       },
-      sync_with: [
-        'j.w',
-        'k.a',
-        'j.z',
-        'd.h'
-      ]
+      departments: ['Help1', 'Help2', 'Help3']
     }
   },
-  computed: {
-  },
+  computed: {},
   methods: {
-    submit () {
-
-    },
+    submit () {},
     reset () {
-      this.form = mapValues(this.form, item => {
+      this.form = mapValues(this.form, (item) => {
         if (item && typeof item === 'object') {
           return []
         }
@@ -93,7 +85,7 @@ export default {
       })
     },
     synced () {
-      this.form = mapValues(this.form, item => {
+      this.form = mapValues(this.form, (item) => {
         if (item && typeof item === 'object') {
           return []
         }
