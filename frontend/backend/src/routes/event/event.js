edab5e7450b86e8ddc2d9fa600850b7b46593534
Adrian Gonzalez <agonzalez108@miners.utep.edu>
2020-10-11 20:43:11 -0600

    [DO NOT MERGE] integrate with current frontend with backend

---
+++ frontend/backend/src/routes/event/event.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { eventController } from '../../controllers'
+
+// TODO: define specific endpoint for events here
+export const router = express.Router({
+  strict: true
+})
+
+router.post('/', (req, res) => {
+  eventController.create(req, res)
+})
+
+router.get('/all', (req, res) => {
+  eventController.findAllEvents(req, res)
+})
+
+router.get('/:eventId', (req, res) => {
+  eventController.read(req, res)
+})
+
+router.patch('/:eventId', (req, res) => {
+  eventController.update(req, res)
+})
+
+router.delete('/:eventId', (req, res) => {
+  eventController.delete(req, res)
+})
