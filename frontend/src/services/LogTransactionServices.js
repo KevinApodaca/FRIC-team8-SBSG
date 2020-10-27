3f1b99a021b5d8bf46cb20749ed3b17acf7ab53e
Adrian Gonzalez <agonzalez108@miners.utep.edu>
2020-10-26 18:22:11 -0600

    Now you can view transactions

--- frontend/src/services/LogTransactionServices.js
+++ frontend/src/services/LogTransactionServices.js
@@ -4,5 +4,10 @@ export default {
   async logAction (transaction) {
     const res = await axios.post('http://localhost:3000/transactions/', transaction)
     return res
+  },
+
+  async getTransactions () {
+    const res = await axios.get('http://localhost:3000/transactions/all')
+    return res
   }
 }
--- frontend/src/views/CreateEvent.vue
+++ frontend/src/views/CreateEvent.vue
@@ -159,7 +159,7 @@ export default {
     async logAction () {
       var trans = {
         initials: 'K.A',
-        action: 'K.A created ' + this.form.name
+        action: 'K.A created event' + this.form.name
       }
       LogServices.logAction(trans)
         .then(response => {
