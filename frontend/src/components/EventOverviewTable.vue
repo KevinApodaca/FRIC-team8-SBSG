a2531b5bf877ad37a88922381b2de3fb0cd2a96f
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-10-27 14:36:18 -0600

    [FRIC-73] fixed from old branch

--- frontend/src/components/EventOverviewTable.vue
+++ frontend/src/components/EventOverviewTable.vue
@@ -38,8 +38,8 @@
           <router-link :to="{name:'client.edit', params: {id: props.row.id}}" class="button is-small is-primary" v-b-tooltip.hover title="Event Detailed View">
             <b-icon icon="information" size="is-small"/>
           </router-link>
-          <button class="button is-small is-info" type="button" @click.prevent="trashModal(props.row)" v-b-tooltip.hover title="Delete Event">
-            <b-icon icon="trash-can" size="is-small"/>
+          <button class="button is-small is-info" type="button" @click.prevent="trashModal(props.row)" v-b-tooltip.hover title="Archive Event">
+            <b-icon icon="archive" size="is-small"/>
           </button>
         </div>
       </b-table-column>
