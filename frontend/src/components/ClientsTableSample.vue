623e5bdded9f072305a204ae6764de1d6658e77c
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-10-27 08:34:11 -0600

    [FRIC-76] make the archive icons blue

--- frontend/src/components/ClientsTableSample.vue
+++ frontend/src/components/ClientsTableSample.vue
@@ -38,7 +38,7 @@
           <router-link :to="{name:'client.edit', params: {id: props.row.id}}" class="button is-small is-primary" v-b-tooltip.hover title="Event Detailed View">
             <b-icon icon="information" size="is-small"/>
           </router-link>
-          <button class="button is-small is-danger" type="button" @click.prevent="trashModal(props.row)" v-b-tooltip.hover title="Delete Event">
+          <button class="button is-small is-info" type="button" @click.prevent="trashModal(props.row)" v-b-tooltip.hover title="Delete Event">
             <b-icon icon="trash-can" size="is-small"/>
           </button>
         </div>
