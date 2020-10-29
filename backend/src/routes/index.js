4b6fbcd752a581778a569033e7fce265c138e317
Adrian Gonzalez <agonzalez108@miners.utep.edu>
2020-10-28 22:53:01 -0600

    Added fininding to backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,11 +3,13 @@ import { router as systemRouter } from './system/system'
 import { router as transactionRouter } from './transaction/transaction'
 import { router as taskRouter } from './task/task'
 import { router as subtaskRouter } from './subtask/subtask'
+import { router as findingRouter } from './finding/finding'
 
 export {
   eventRouter,
   systemRouter,
   transactionRouter,
   taskRouter,
-  subtaskRouter
+  subtaskRouter,
+  findingRouter
 }
