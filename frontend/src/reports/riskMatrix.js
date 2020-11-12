70297f87737750cf47782e9a6ae6be7fe5edca3d
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-11-12 10:27:12 -0700

    Revert "Merge pull request #109 from KevinApodaca/FRIC-54-useOfficegenForReports"
    This reverts commit 425d8c36bb427f653c40f9655a675b8e63fd7ace, reversing
    changes made to b30a1f5bba74fee5b548a543231d91938a7862ba.

--- frontend/src/reports/riskMatrix.js
+++
@@ -1,9 +0,0 @@
-const officegen = require('officegen')
-const fs = require('fs')
-
-/* TODO grab all findings and systems from db */
-var findings = ''
-var systems = ''
-
-let xlsx = officegen('xlsx')
-
