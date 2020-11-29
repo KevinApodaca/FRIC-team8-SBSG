4701956bb1c35e8d0eb859d221b77e06677ba12f
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-11-29 14:12:34 -0700

    :zap: can now download files from the button and have success alert

--- frontend/src/views/ExportData.vue
+++ frontend/src/views/ExportData.vue
@@ -9,10 +9,10 @@
         </notification>
         <card-component class="has-table has-mobile-sort-spaced" title="Export Data" icon="share">
        <div class="buttons">
-            <b-button type="is-link is-rounded" expanded>Systems</b-button>
-            <b-button type="is-danger is-rounded" expanded>Tasks</b-button>
-            <b-button type="is-warning is-rounded" expanded>Subtasks</b-button>
-            <b-button type="is-success is-rounded" expanded>Findings</b-button>
+            <b-button type="is-link is-rounded" expanded @click="onExportFinish('systems')">Systems</b-button>
+            <b-button type="is-danger is-rounded" expanded @click="onExportFinish('tasks')">Tasks</b-button>
+            <b-button type="is-warning is-rounded" expanded @click="onExportFinish('subtasks')">Subtasks</b-button>
+            <b-button type="is-success is-rounded" expanded @click="onExportFinish('findings')">Findings</b-button>
             <b-button type="is-primary is-rounded" expanded>All</b-button>
         </div>
         </card-component>
@@ -24,15 +24,16 @@
 import CardComponent from '@/components/CardComponent'
 import Notification from '@/components/Notification'
 import swal from 'sweetalert'
-
+import FileSaver from 'file-saver'
 export default {
   name: 'Expor',
   components: { CardComponent, Notification },
   computed: {
   },
   methods: {
-    onExportFinish () {
-      swal('Export finsihed', 'success')
+    async onExportFinish (artifact) {
+      await FileSaver.saveAs('http://localhost:3000/' + artifact + '/all', `${artifact}.json`)
+      swal('Done', 'Data was exported and archived', 'success')
     }
   }
 }
