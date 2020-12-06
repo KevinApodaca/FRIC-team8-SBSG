14d2ab4d68ea4526aad5243e06cb83971504750c
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-12-06 12:44:04 -0700

    update finding services to use env vars

--- frontend/src/services/FindingServices.js
+++ frontend/src/services/FindingServices.js
@@ -1,34 +1,35 @@
 import axios from 'axios'
 const qs = require('qs')
+const hostname = process.env.VUE_APP_API_HOST
 
 export default {
   async getFindings () {
-    const res = await axios.get('http://localhost:3000/findings/all')
+    const res = await axios.get(`http://${hostname}:3000/findings/all`)
     return res
   },
 
   async getFindingSingle (findingId) {
-    const res = await axios.get('http://localhost:3000/findings/' + findingId)
+    const res = await axios.get(`http://${hostname}:3000/findings/` + findingId)
     return res
   },
 
   async createFinding (form) {
-    const res = await axios.post('http://localhost:3000/findings/', form)
+    const res = await axios.post(`http://${hostname}:3000/findings/`, form)
     return res
   },
 
   async deleteFinding (findingId) {
-    const res = await axios.delete('http://localhost:3000/findings/' + findingId)
+    const res = await axios.delete(`http://${hostname}:3000/findings/` + findingId)
     return res
   },
 
   async modifyFinding (findingId, newForm) {
-    const res = await axios.patch('http://localhost:3000/findings/' + findingId, newForm)
+    const res = await axios.patch(`http://${hostname}:3000/findings/` + findingId, newForm)
     return res
   },
 
   async getMultipleFindings (multipleFindings) {
-    const res = await axios.get('http://localhost:3000/findings/multiple/findings', {
+    const res = await axios.get(`http://${hostname}:3000/findings/multiple/findings`, {
       params: {
         arr: multipleFindings
       },
