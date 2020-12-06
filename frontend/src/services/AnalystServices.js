5d75d7ae38a5de92d4b32b95463a2d719544413f
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-12-06 12:34:06 -0700

    update analyst serv to use env vars

--- frontend/src/services/AnalystServices.js
+++ frontend/src/services/AnalystServices.js
@@ -1,34 +1,35 @@
 import axios from 'axios'
 const qs = require('qs')
+const hostname = process.env.VUE_APP_API_HOST
 
 export default {
   async getAnalysts () {
-    const res = await axios.get('http://localhost:3000/analysts/all')
+    const res = await axios.get(`http://${hostname}:3000/analysts/all`)
     return res
   },
 
   async getAnalystSingle (analystId) {
-    const res = await axios.get('http://localhost:3000/analysts/' + analystId)
+    const res = await axios.get(`http://${hostname}:3000/analysts/` + analystId)
     return res
   },
 
   async createAnalyst (form) {
-    const res = await axios.post('http://localhost:3000/analysts/', form)
+    const res = await axios.post(`http://${hostname}:3000/analysts/`, form)
     return res
   },
 
   async deleteAnalyst (analystId) {
-    const res = await axios.delete('http://localhost:3000/analysts/' + analystId)
+    const res = await axios.delete(`http://${hostname}:3000/analysts/` + analystId)
     return res
   },
 
   async modifyAnalyst (newForm, analystId) {
-    const res = await axios.patch('http://localhost:3000/analysts/' + analystId, newForm)
+    const res = await axios.patch(`http://${hostname}:3000/analysts/` + analystId, newForm)
     return res
   },
 
   async getAnalystFromEvent (listOfAnalyst) {
-    const res = await axios.get('http://localhost:3000/analysts/listOfAnalyst', {
+    const res = await axios.get(`http://${hostname}:3000/analysts/listOfAnalyst`, {
       params: {
         arr: listOfAnalyst
       },
