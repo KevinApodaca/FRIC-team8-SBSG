fbc31ac0642aae2edc37a3aede3d401712407370
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-12-06 12:54:26 -0700

    update system services with env vars

--- frontend/src/services/SystemServices.js
+++ frontend/src/services/SystemServices.js
@@ -1,70 +1,71 @@
 import axios from 'axios'
 const qs = require('qs')
+const hostname = process.env.VUE_APP_API_HOST
 
 export default {
   async getSystems () {
-    const res = await axios.get('http://localhost:3000/systems/all')
+    const res = await axios.get(`http://${hostname}:3000/systems/all`)
     return res
   },
 
   async getSystemSingle (systemId) {
-    const res = await axios.get('http://localhost:3000/systems/' + systemId)
+    const res = await axios.get(`http://${hostname}:3000/systems/` + systemId)
     return res
   },
 
   async createSystem (form) {
-    const res = await axios.post('http://localhost:3000/systems/', form)
+    const res = await axios.post(`http://${hostname}:3000/systems/`, form)
     return res
   },
 
   async deleteSystem (systemId) {
-    const res = await axios.delete('http://localhost:3000/systems/' + systemId)
+    const res = await axios.delete(`http://${hostname}:3000/systems/` + systemId)
     return res
   },
 
   async modifySystem (systemId, newForm) {
-    const res = await axios.patch('http://localhost:3000/systems/' + systemId, newForm)
+    const res = await axios.patch(`http://${hostname}:3000/systems/` + systemId, newForm)
     return res
   },
 
   async addTask (systemId, taskId) {
     const task = { task_association: taskId }
-    const res = await axios.patch('http://localhost:3000/systems/addItem/' + systemId, task)
+    const res = await axios.patch(`http://${hostname}:3000/systems/addItem/` + systemId, task)
     return res
   },
 
   async addSubtask (systemId, subtaskId) {
     const subtask = { subtask_association: subtaskId }
-    const res = await axios.patch('http://localhost:3000/systems/addItem/' + systemId, subtask)
+    const res = await axios.patch(`http://${hostname}:3000/systems/addItem/` + systemId, subtask)
     return res
   },
 
   async addFinding (systemId, findingId) {
     const finding = { finding_association: findingId }
-    const res = await axios.patch('http://localhost:3000/systems/addItem/' + systemId, finding)
+    const res = await axios.patch(`http://${hostname}:3000/systems/addItem/` + systemId, finding)
     return res
   },
 
   async removeTask (systemId, taskId) {
     const task = { task_association: taskId }
-    const res = await axios.patch('http://localhost:3000/systems/removeItem/' + systemId, task)
+    const res = await axios.patch(`http://${hostname}:3000/systems/removeItem/` + systemId, task)
     return res
   },
 
   async removeSubtask (systemId, subtaskId) {
     const subtask = { subtask_association: subtaskId }
-    const res = await axios.patch('http://localhost:3000/systems/removeItem/' + systemId, subtask)
+    const res = await axios.patch(`http://${hostname}:3000/systems/removeItem/` + systemId, subtask)
     return res
   },
 
   async removeFinding (systemId, findingId) {
     const finding = { finding_association: findingId }
-    const res = await axios.patch('http://localhost:3000/systems/removeItem/' + systemId, finding)
+    const res = await axios.patch(`http://${hostname}:3000/systems/removeItem/` + systemId, finding)
     return res
   },
 
   async getMultipleSystems (multipleSystems) {
-    const res = await axios.get('http://localhost:3000/systems/multiple/systems', {
+    const res = await axios.get(`http://${hostname}:3000/systems/multiple/systems`, {
       params: {
         arr: multipleSystems
       },
