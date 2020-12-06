6029d710b84ba62ada4a3f77fac3aa5d4a07981f
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-12-06 12:56:08 -0700

    update task serv to use env vars

--- frontend/src/services/TaskServices.js
+++ frontend/src/services/TaskServices.js
@@ -1,70 +1,71 @@
 import axios from 'axios'
 const qs = require('qs')
+const hostname = process.env.VUE_APP_API_HOST
 
 export default {
   async getTasks () {
-    const res = await axios.get('http://localhost:3000/tasks/all')
+    const res = await axios.get(`http://${hostname}:3000/tasks/all`)
     return res
   },
 
   async getTaskSingle (taskId) {
-    const res = await axios.get('http://localhost:3000/tasks/' + taskId)
+    const res = await axios.get(`http://${hostname}:3000/tasks/` + taskId)
     return res
   },
 
   async createTask (form) {
-    const res = await axios.post('http://localhost:3000/tasks/', form)
+    const res = await axios.post(`http://${hostname}:3000/tasks/`, form)
     return res
   },
 
   async deleteTask (taskId) {
-    const res = await axios.delete('http://localhost:3000/tasks/' + taskId)
+    const res = await axios.delete(`http://${hostname}:3000/tasks/` + taskId)
     return res
   },
 
   async modifyTask (taskId, newForm) {
-    const res = await axios.patch('http://localhost:3000/tasks/' + taskId, newForm)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/` + taskId, newForm)
     return res
   },
 
   async addTask (taskId, task2Id) {
     const task = { task_association: task2Id }
-    const res = await axios.patch('http://localhost:3000/tasks/addItem/' + taskId, task)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/addItem/` + taskId, task)
     return res
   },
 
   async addSubtask (taskId, subtaskId) {
     const subtask = { subtask_association: subtaskId }
-    const res = await axios.patch('http://localhost:3000/tasks/addItem/' + taskId, subtask)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/addItem/` + taskId, subtask)
     return res
   },
 
   async addFinding (taskId, findingkId) {
     const finding = { finding_association: findingkId }
-    const res = await axios.patch('http://localhost:3000/tasks/addItem/' + taskId, finding)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/addItem/` + taskId, finding)
     return res
   },
 
   async removeTask (taskId, task2Id) {
     const task = { task_association: task2Id }
-    const res = await axios.patch('http://localhost:3000/tasks/removeItem/' + taskId, task)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/removeItem/` + taskId, task)
     return res
   },
 
   async removeSubtask (taskId, subtaskId) {
     const subtask = { subtask_association: subtaskId }
-    const res = await axios.patch('http://localhost:3000/tasks/removeItem/' + taskId, subtask)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/removeItem/` + taskId, subtask)
     return res
   },
 
   async removeFinding (taskId, findingkId) {
     const finding = { finding_association: findingkId }
-    const res = await axios.patch('http://localhost:3000/tasks/removeItem/' + taskId, finding)
+    const res = await axios.patch(`http://${hostname}:3000/tasks/removeItem/` + taskId, finding)
     return res
   },
 
   async getMultipleTasks (multipleTasks) {
-    const res = await axios.get('http://localhost:3000/tasks/multiple/tasks', {
+    const res = await axios.get(`http://${hostname}:3000/tasks/multiple/tasks`, {
       params: {
         arr: multipleTasks
       },
