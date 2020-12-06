20dedf14feada639510361edbbd90f0596496c22
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-12-06 12:16:24 -0700

    add env variabel for mongo hostname

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -2,10 +2,11 @@
  * Port of the Expresss server
  */
 export const PORT = process.env.PORT || 3000
+export const DATABSE_HOSTNAME = process.env.DATABASE_HOSTNAME || 'localhost'
 
 /**
  * Configuration for MongoDB
  */
 export const dbConfig = {
-  url: 'mongodb://localhost:27017/db'
-}
+  url: `mongodb://${DATABSE_HOSTNAME}:27017/db`
+}`
